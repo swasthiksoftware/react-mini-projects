f7eeda01eb2720882e29419dc35e510449312731
Srinivasan <swasthiksoftware@gmail.com>
2026-01-04 18:36:38 +0530

    new component added

---
+++ src/components/Posts.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import './styles/postscss.css';
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+const [loading, setLoading] = useState(true);
+  useEffect(() => {
+  fetch("https://your-backend-url.onrender.com/api/posts")
+    .then(res => res.json())
+    .then(data => {
+      setPosts(data);
+      setLoading(false);
+    });
+}, []);
+if (loading) return <p>Loading...</p>;
+if (posts.length === 0) return <p>No posts found</p>;
+  return (
+    <div>
+      <h2>Latest Posts</h2>
+
+      {posts.map(post => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
